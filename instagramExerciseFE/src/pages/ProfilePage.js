bc4dd9d5275bdc65dc968ec3860a7e524125232f
haimy88 <joshuahaimowitz@gmail.com>
2022-05-24 22:28:39 +0300

    formatting on front end

--- instagramExerciseFE/src/pages/ProfilePage.js
+++ instagramExerciseFE/src/pages/ProfilePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar.tsx";
+import NavBar_bottom_fixed from "../components/NavBar_bottom_fixed.tsx";
 
 export default function ProfilePage() {
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
+      <NavBar_bottom_fixed highlight={"/profile"} />
     </>
   );
 }
